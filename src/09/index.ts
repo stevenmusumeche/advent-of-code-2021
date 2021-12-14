3b5a56c717f4b9a9b4170569779c5c048483fab4
Steven Musumeche <steven@musumeche.com>
2021-12-14 17:12:27 -0600

    day 9 part 2

--- src/09/index.ts
+++ src/09/index.ts
@@ -4,17 +4,65 @@ export function runA(inputPath: string): any {
   const grid = parseInput(inputPath);
   const lowPoints = findLowPoints(grid);
   return lowPoints.reduce((acc, cur) => {
-    return (acc += grid[cur[0]][cur[1]] + 1);
+    return (acc += grid[cur[0]][cur[1]].val + 1);
   }, 0);
 }
 
-export function runB(inputPath: string): any {}
+export function runB(inputPath: string): any {
+  const grid = parseInput(inputPath);
+  const lowPoints = findLowPoints(grid);
+  const basinSizes = lowPoints
+    .map((lowPoint) => explore(lowPoint, grid))
+    .sort((a, b) => b - a);
+  const threeLargestBasinSizes = basinSizes.splice(0, 3);
+  return threeLargestBasinSizes.reduce((acc, cur) => {
+    return cur * acc;
+  }, 1);
+}
+
+function explore(point: Point, grid: Grid) {
+  grid[point[0]][point[1]].visited = true;
+  let basinSize = 1;
+
+  const right: Point = [point[0], point[1] + 1];
+  if (isAllowed(right, grid)) {
+    basinSize += explore(right, grid);
+  }
+
+  const down: Point = [point[0] + 1, point[1]];
+  if (isAllowed(down, grid)) {
+    basinSize += explore(down, grid);
+  }
+
+  const left: Point = [point[0], point[1] - 1];
+  if (isAllowed(left, grid)) {
+    basinSize += explore(left, grid);
+  }
+
+  const up: Point = [point[0] - 1, point[1]];
+  if (isAllowed(up, grid)) {
+    basinSize += explore(up, grid);
+  }
+
+  return basinSize;
+}
+
+function isAllowed(point: Point, grid: Grid) {
+  return (
+    point[0] < grid.length &&
+    point[0] >= 0 &&
+    point[1] < grid[0].length &&
+    point[1] >= 0 &&
+    grid[point[0]][point[1]].visited == false &&
+    grid[point[0]][point[1]].val !== 9
+  );
+}
 
 function findLowPoints(grid: Grid): Point[] {
   const lowPoints: Point[] = [];
   for (let r = 0; r < grid.length; r++) {
     for (let c = 0; c < grid[0].length; c++) {
-      const val = grid[r][c];
+      const val = grid[r][c].val;
       const adjacent = getAdjacentValues(r, c, grid);
       const isLowest = adjacent.every((a) => a > val);
       if (isLowest) {
@@ -28,7 +76,7 @@ function findLowPoints(grid: Grid): Point[] {
 
 function getAdjacentValues(row: number, col: number, grid: Grid): number[] {
   const points = getAdjacentIndexes(row, col, grid);
-  return points.map((point) => grid[point[0]][point[1]]);
+  return points.map((point) => grid[point[0]][point[1]].val);
 }
 
 function getAdjacentIndexes(row: number, col: number, grid: Grid): Point[] {
@@ -52,8 +100,11 @@ function getAdjacentIndexes(row: number, col: number, grid: Grid): Point[] {
 function parseInput(inputPath: string): Grid {
   const lines = linesToArray(inputPath);
 
-  return lines.map((line) => line.split("").map(Number));
+  return lines.map((line) =>
+    line.split("").map((x) => ({ val: Number(x), visited: false }))
+  );
 }
 
-type Grid = number[][];
+type GridVal = { val: number; visited: boolean };
+type Grid = GridVal[][];
 type Point = [number, number];
